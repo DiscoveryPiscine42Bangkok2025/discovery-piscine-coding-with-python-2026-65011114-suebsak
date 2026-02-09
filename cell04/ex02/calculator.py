655c971f35a56233c432d8f53b19ab521a112c11
unknown <65011114@kmitl.ac.th>
2026-02-09 22:18:53 +0700

    ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+try:
+    
+    num1 = int(input("Give me the first number: "))
+    num2 = int(input("Give me the second number: "))
+
+    
+    print("Thank you!")
+
+    
+    print(f"{num1} + {num2} = {num1 + num2}")
+    print(f"{num1} - {num2} = {num1 - num2}")
+    
+    
+    print(f"{num1} / {num2} = {int(num1 / num2)}")
+    
+    print(f"{num1} * {num2} = {num1 * num2}")
+
+except ValueError:
+    print("Error: Please enter valid integers.")
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero.")

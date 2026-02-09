54ed4c73e460f01570cf521b5b104e2a536a8ac7
unknown <65011114@kmitl.ac.th>
2026-02-10 01:05:45 +0700

    ex02

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+
+
+def downcase_it(string_in):
+    return string_in.lower()
+
+def main():
+    
+    if len(sys.argv) > 1:
+        
+        for arg in sys.argv[1:]:
+            
+            print(downcase_it(arg))
+    else:
+        
+        print("none")
+
+if __name__ == "__main__":
+    main()

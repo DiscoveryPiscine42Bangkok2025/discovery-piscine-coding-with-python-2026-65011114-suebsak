2ffa7f1a75f767d80ba84211e9e81bba2956d562
unknown <65011114@kmitl.ac.th>
2026-02-09 22:43:29 +0700

    ex04

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+
+try:
+    
+    user_input = input("Give me a number: ")
+    number = float(user_input)
+
+    
+    result = math.ceil(number)
+
+    
+    print(result)
+
+except ValueError:
+    print("Error: Please enter a valid number.")
